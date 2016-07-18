20a02e1a8e8859dafa804343d23c8514b00f57ed
Kaio <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2016-07-18 15:46:50 -0300

    Mudanca de int pra Integer

--- linear_sorting/src/main/java/sorting/Counting.java
+++ linear_sorting/src/main/java/sorting/Counting.java
@@ -1,30 +1,47 @@
-package sorting;
+package sorting.linearSorting;
 
-public class Counting {
-	
-	public void sort(int[] array, int leftIndex, int rightIndex) {
+import sorting.AbstractSorting;
+
+/**
+ * Classe que implementa a estratégia de Counting Sort vista em sala. Procure evitar desperdicio de 
+ * memoria alocando o array de contadores com o tamanho sendo o máximo inteiro presente no array 
+ * a ser ordenado.  
+ * 
+ */
+public class CountingSort extends AbstractSorting<Integer> {
+
+	@Override
+	public void sort(Integer[] array,int leftIndex, int rightIndex) {
 		
-		if (array == null || array.length == 0 || (leftIndex - rightIndex) <= 0)
+		if (array == null || array.length == 0)
 			return;
 		
-		int maiorElemento = procuraMaiorElemento(array, leftIndex, rightIndex);
-		int[] count = new int[maiorElemento + 1];
+		int maiorElemento = searchBiggestElement(array);
+		Integer[] count = new Integer[maiorElemento + 1];
 		
+		// transforma null em 0
+		for (int i = 0; i < count.length; i++) {
+			if (count[i] == null)
+				count[i] = 0;
+		}
+		
+		// conta
 		for (int i = leftIndex; i <= rightIndex; i++) {
 			count[array[i]]++;
 		}
 		
-		for (int i = 0, j = rightIndex; i < count.length; i++) {
+		// joga no vetor original
+		for (int i = 0, j = leftIndex; i < count.length; i++) {
 			while (count[i]-- > 0)
 				array[j++] = i;
 		}
 		
 	}
 	
-	public int procuraMaiorElemento(int[] array, int leftIndex, int rightIndex) {
+	public int searchBiggestElement(Integer[] array) {
 		int maior = array[0];
 		
-		for (int i = leftIndex + 1; i <= rightIndex; i++) {
+		for (int i = 0; i < array.length; i++) {
 			if (array[i] > maior)
 				maior = array[i];
 		}
